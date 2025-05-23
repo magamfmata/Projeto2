6d0eba1396d773cf5d64be8c8ad63aa287ebb083
Magali <magamayrmata@gmail.com>
2025-05-23 17:49:30 +0100

    IIgredient and Igredient Fixed

--- MealPlanner/Projeto2v2/MealPlanner/Controller/Ingredient.cs
+++ MealPlanner/Projeto2v2/MealPlanner/Controller/Ingredient.cs
@@ -5,8 +5,36 @@ using System.Threading.Tasks;
 
 namespace MealPlanner.Projeto2v2.MealPlanner.Controller
 {
-    public class Ingredient
+using System;
+
+namespace MealPlanner.Model
+{
+    public class Ingredient : IIngredient
     {
-        
+        public string Name { get; }
+        public string Type { get; }
+
+        public Ingredient(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Ingredient);
+        }
+
+        public bool Equals(Ingredient other)
+        {
+            if (other is null) return false;
+            return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase)
+                && string.Equals(Type, other.Type, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name?.ToLowerInvariant(), Type?.ToLowerInvariant());
+        }
     }
 }
--- MealPlanner/Projeto2v2/MealPlanner/Controller/Recipe.cs
+++ MealPlanner/Projeto2v2/MealPlanner/Controller/Recipe.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 namespace MealPlanner.Controller
 {
     public class Recipe : IRecipe
-    {
-        
+{
+    public string Nome { get; }
+    public List<Ingredient> Ingredients { get; }
 
-        }
+    public Recipe(string name, List<Ingredient> ingredients)
+    {
+        Name = name;
+        Ingredients = ingredientss;
+    }
+
+    public int CompareTo(Recipe other)
+    {
+        return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
     }
 }
+
